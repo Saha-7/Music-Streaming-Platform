1feaf6aca47b45d57fd278fe8aa18a3860ad3fc6
saha-7op <saha7pritam@gmail.com>
2025-11-25 14:39:50 +0530

    Static files added

---
+++ frontend/src/components/Sidebar.tsx
@@ -0,0 +1,10 @@
+
+const Sidebar = () => {
+  return (
+    <div className="w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex">
+      Sidebar
+    </div>
+  )
+}
+
+export default Sidebar
